ec0aac7514379f57c23c6749c349cf591342ad4d
Kush-316 <kushagrakapoor619@gmail.com>
2025-03-26 00:59:22 +0530

    signup and login func made

--- models/models.go
+++ models/models.go
@@ -5,10 +5,10 @@ import(
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type user struct{
+type User struct{
 	ID				primitive.ObjectID	`json:"_id" bson:"_id"`
 	First_Name		*string				`json:"first_name" validate:"required,min=2,max=30"`
-	Last_Name		*string				`json:"last_name validate:"required,min=2,max=30"`
+	Last_Name		*string				`json:"last_name" validate:"required,min=2,max=30"`
 	Password		*string				`json:"password" validate:"required,min=6"`
 	Email			*string				`json:"email" validate:"email,required"`
 	Phone			*string				`json:"phone" validate:"required"`
@@ -43,14 +43,14 @@ type Address struct{
 	House			*string				`json:"house_name" bson:"house_name"`
 	Street			*string				`json:"street_name" bson:"street_name"`
 	City			*string				`json:"city_name" bson:"city_name"`
-	Pincode			*string				`json:"pin_code" bson:pin_code"`
+	Pincode			*string				`json:"pin_code" bson:"pin_code"`
 }
 
 type Order struct{
 	Order_ID		primitive.ObjectID	`bson:"_id"`
 	Order_Cart		[]ProductUser		`json:"order_list" bson:"order_list"`
 	Ordered_At		time.Time			`json:"ordered_at" bson:"ordered_at"`
-	Price			int					`json:"total_price bson:"total_price""`
+	Price			int					`json:"total_price" bson:"total_price"`
 	Discount		*int				`json:"discount" bson:"discount"`
 	Payment_Method	Payment				`json:"payment_method" bson:"payment_method"`
 }
